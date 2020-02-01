ea49869a6cdf23412d570846a933fbe6756be978
Sean Halupczynski <sean.halup@gmail.com>
2020-02-01 14:26:55 -0500

    deployed to heroku and created mLab db on heroku

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: true
